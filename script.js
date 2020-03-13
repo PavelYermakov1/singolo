b02ed1a9ff46346889809d01c476a790d023823e
Pavel Yermakov <pavelak137@mail.ru>
2020-03-13 15:28:01 +0300

    feat: add border pictures

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('world');
+const state = {
+
+};
+const body = document.body;
+
+
+const potfolioNavigation = body.querySelectorAll('.tag')
+for (let elem of potfolioNavigation) {
+    elem.addEventListener("click", (event) => {
+        target = event.target;
+        potfolioNavigation.forEach(e => e.classList.remove("tag_selected"));
+        target.classList.toggle("tag_selected");  
+    });
+}
+
+
+const findPictures = body.querySelectorAll('.layout-4-column > img');
+for(let elem of findPictures) {
+    elem.addEventListener("click", (event) => {
+        target = event.target;
+        if(target.classList.contains("bordered")){
+            findPictures.forEach(e => e.classList.remove("bordered"));  
+        }else {
+            findPictures.forEach(e => e.classList.remove("bordered"));
+            target.classList.toggle("bordered");
+        }
+    });
+}
+
+findPictures[0].src = "./assets/image/Picture-8.png";
+console.log(findPictures[0].src);
